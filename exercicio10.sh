842eb92c90fecaaf4304c99a460da49737f083c5
SuelanyBr <maria.cruz@ccc.ufcg.edu.br>
2016-09-13 15:22:52 -0300

    Exercicio 10

--- exercicio10.sh
+++ exercicio10.sh
@@ -6,31 +6,70 @@
 
 
 ARCHIVE_INITIAL=$1
-TYPE_1="CRC"
-TYPE_3="MD5"
-TYPE_2="SHA1"
+
+
+type_default(){
+	FILE=$1
+	OLD_CHECKSUM=$2
+	echo "$FILE	$OLD_CHECKSUM"
+	
+	
+	CHECKSUM_1=$( cksum $FILE | cut -d" " -f1 )
+	CHECKSUM_2=$( md5sum $FILE | cut -d" " -f1 )
+	CHECKSUM_3=$( sha1sum $FILE | cut -d" " -f1 )
+			
+	if [ "$CHECKSUM_1" == "$OLD_CHECKSUM" ]; then
+		echo $CHECKSUM_1
+		
+	elif [ "$CHECKSUM_2" == "$OLD_CHECKSUM" ]; then
+		echo $CHECKSUM_2
+		
+	elif [ "$CHECKSUM_3" == "$OLD_CHECKSUM" ]; then
+	 	echo $CHECKSUM_3
+	 	
+	else
+		echo "NOT_FOUND $FILE"
+	
+	fi	
+}
 
 while read LINHA; do
 	# Colunas de cada linha do arquivo geral
 	CHECKSUM=$( echo $LINHA | cut -d" " -f1 )
 	TYPE_ALGORITHM=$( echo $LINHA | cut -d" " -f2 )
-	ARCHIVE=$( echo $LINHA | cut -d" " -f3- )
+	FILE=$( echo $LINHA | cut -d" " -f3- )
 
-	elif [ TYPE_ALGORITHM == TYPE_1 ]; then
+	# Testa existencia de um arquivo
+	if [ -f "$FILE" ]; then
+		
+		NEW_CHECKSUM=""
+		
+		# Gera nova checagem a partir do algoritmo passado como parametro
+		case $TYPE_ALGORITHM in
+		
+			"CRC") NEW_CHECKSUM=$( cksum $FILE | cut -d" " -f1 ) ;;
 			
+			"MD5") NEW_CHECKSUM=$( md5sum $FILE | cut -d" " -f1 ) ;;
 			
-	elif [ TYPE_ALGORITHM == TYPE_2 ]; then
+			"SHA1") NEW_CHECKSUM=$( sha1sum $FILE | cut -d" " -f1);;
+			
+			# Testa para todos os outros algoritmos
+			*) NEW_CHECKSUM= type_default "$FILE $CHECKSUM" ;;
+		esac
+		
+		# Compara os valores das checagens
+		echo $NEW_CHECKSUM
+		if [ "$CHECKSUM" == "$NEW_CHECKSUM" ]; then
+			echo "OK $FILE" 
+		else
+			echo "NOT_FOUND $FILE"
+		fi
 	
-	
-	elif [ TYPE_ALGORITHM == TYPE_3 ]; then
-	
-	
+	# Se arquivo nao existe		
 	else
+		echo "ERROR: $FILE NOT EXIST"
 	
-	
-	fi
-	
-	
+	fi	
 
 
 done < $ARCHIVE_INITIAL
